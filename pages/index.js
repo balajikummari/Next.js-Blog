dc2c58c5e4786a141dce55e24a9d3e90bff0f291
Balaji Kummari <balajievlo@gmail.com>
2020-04-23 17:30:07 +0530

    Pro pic update

--- pages/index.js
+++ pages/index.js
@@ -12,9 +12,9 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>Nahh!!!</p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
+          {' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
